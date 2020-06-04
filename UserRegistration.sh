5aeeb05cd4777017f0d58150036064c9404394b0
unknown <neerajkumar8546@gmail.com>
2020-06-04 11:47:11 +0530

    emaail validate

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,10 +1,22 @@
-#!/bin/bash -x
-pat=^[[:upper:]][[:lower:]]*$ 
-read a
-if [[ $a =~ $pat ]]
+#! /bin/bash -x
+shopt -s extglob
+read -p "Enter a email:" email;
+regex="^[a-z0-9]+([._+-][0-9a-z]+)@+[a-z0-9]+.[a-z]{2,4}([.][a-z]{2}$)"
+regex1="^[a-z0-9]+([._+-][0-9a-z]+)@+[a-z0-9]+.[a-z]{2,4}$"
+regex2="(^[a-z0-9]+)@+[a-z0-9]+.[a-z]{2,4}([.][a-z]{2}$)"
+regex4="(^[a-z0-9]+)@+[a-z0-9]+.[a-z]{2,4}$"
+if [[ $email =~ $regex ]]
 then
-      echo "valid Last Name"
+	echo "valid"
+elif [[ $email =~ $regex1 ]]
+then
+        echo "valid"
+elif [[ $email =~ $regex2 ]]
+then
+	echo "valid"
+elif [[ $email =~ $regex4 ]]
+then
+        echo "valid"
 else
-   echo "Invalid Last Name"
+        echo "not valid"
 fi
-
